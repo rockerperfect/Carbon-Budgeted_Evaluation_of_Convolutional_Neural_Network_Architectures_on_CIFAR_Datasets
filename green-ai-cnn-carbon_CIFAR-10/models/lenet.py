39c944a49a53c175f09516d386c91ada4efe8671
DIVYE BISARIA <divyebisaria4106@gmail.com>
2026-02-28 22:16:56 +0530

    Update lenet.py

--- green-ai-cnn-carbon_CIFAR-10/models/lenet.py
+++ green-ai-cnn-carbon_CIFAR-10/models/lenet.py
@@ -3,7 +3,7 @@ from tensorflow.keras import layers, models
 
 def build_lenet(num_classes):
     model = models.Sequential([
-        layers.Input(shape=(32, 32, 3)),          # <-- fixes warning too
+        layers.Input(shape=(32, 32, 3)),         
         layers.Conv2D(6, kernel_size=5, activation="relu"),
         layers.AveragePooling2D(pool_size=(2, 2)),
         layers.Conv2D(16, kernel_size=5, activation="relu"),
